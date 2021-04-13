33ef9ad0529c531a542c5a3f72ec5e877283795f
MKDYasserH <khalafaoui.yasser@gmail.com>
2021-04-13 13:49:38 +0200

    Active Learning model now waits for the doccano data to be annotated

--- active_learning/pool_based_AL.py
+++ active_learning/pool_based_AL.py
@@ -1,6 +1,7 @@
 import ast
 
 import numpy as np
+import pandas as pd
 import time
 from modAL.models import ActiveLearner
 from named_entity_recognition_model.spacy_model_class import NerModel, split_data, transform_data
@@ -11,6 +12,46 @@ RANDOM_STATE_SEED = 42
 np.random.seed(RANDOM_STATE_SEED)
 
 data = fetch_data(data_path=DATA_PATH)
+
+
+def split_new_data(data):
+    ''' split the data into a labeled set (seed) and an unlabeled set
+
+    :param data: data containing labeled and unlabeled data
+    :return: dataframe called seed containing all the labeled data and unlabelled_dataset containing unlabelled data
+    '''
+    column_names = data.columns
+    important_columns = ['text', 'labels']
+
+    seed = pd.DataFrame(columns=important_columns)
+    unlabelled_dataset = pd.DataFrame(columns=important_columns)
+
+    for idx, row in data.iterrows():
+        if not row['labels'] :
+            unlabelled_dataset = unlabelled_dataset.append(row[important_columns], ignore_index=True)
+        else :
+            seed = seed.append(row[important_columns], ignore_index=True)
+
+    return seed, unlabelled_dataset
+
+
+def transform_new_data(data):
+    ''' trasnform the labeled dataset into the form ['text', {'entities' : (start_offset, end_offset, label)}]
+
+    :param data: the labeled dataset
+    :return: transformed data
+    '''
+    train_data = []
+    for idx, row in data.iterrows():
+        entities = {}
+        for tag in row['labels']:
+            if 'entities' in entities:
+                entities['entities'].append(tuple(tag))
+            else:
+                entities['entities'] = [tuple(tag)]
+        train_data.append((row['text'], entities))
+    return train_data
+
 def load_split(data):
     '''
 
@@ -44,25 +85,61 @@ N_QUERIES = 20
 # Allow our model to query our unlabeled dataset for the most
 # informative points according to our query strategy (uncertainty sampling)
 for idx in range(N_QUERIES):
-    query_idx, query_instance = learner.query(X_pool)
+    query_idx, query_instance = learner.query(X_pool, n_instances=10)
     print("--- %s seconds ---" % (time.time() - start_time))
+    print(query_idx)
+    print(query_instance)
 
-    X= [X_pool[query_idx[0]]]
+    print("##########  ##########")
+    print("Query n°", idx)
+    confirmation = input("Continue ? (y/n) :")
 
-    # ask the user to label the sentence
-    print("The sentence that needs to be labeled is :")
-    print(X)
-    print("Which words should be labeled ?")
-    labels = input("Format (start_offset, end_offset, 'label') : ")
+    if confirmation.lower() == 'y':
+        # fetch and the new annotated data
+        new_data = fetch_data(data_path='./doccano_data/project_2_dataset.jsonl')
+        new_seed, _ = split_new_data(new_data)
+        new_seed = transform_new_data(new_seed)
 
-    y = np.array([{'entities':[ast.literal_eval(x) for x in labels.splitlines()]}])
-    X = np.array(X)
+        X = []
+        y=[]
+        for sentence in query_instance:
+            for seed in new_seed:
+                if seed[0]==sentence :
+                    X.append(sentence)
+                    y.append(seed[1])
 
-    #Teach our ActiveLearner model the record it has requested
-    learner.teach(X=X, y=y)
+        X = np.array(X)
+        y = np.array(y)
 
-    # remove the queried instance from the unlabeled pool
-    X_pool, y_pool = np.delete(X_pool, query_idx, axis=0), np.delete(y_pool, query_idx)
+        #Teach our ActiveLearner model the record it has requested
+        learner.teach(X=X, y=y)
+
+        # remove the queried instance from the unlabeled pool
+        sorted_query_idx = sorted(query_idx, reverse=True)
+        for index in sorted_query_idx:
+            del X_pool[index]
+
+    else :
+        break
+
+
+
+    # X= [X_pool[query_idx[0]]]
+    #
+    # # ask the user to label the sentence
+    # print("The sentence that needs to be labeled is :")
+    # print(X)
+    # print("Which words should be labeled ?")
+    # labels = input("Format (start_offset, end_offset, 'label') : ")
+    #
+    # y = np.array([{'entities':[ast.literal_eval(x) for x in labels.splitlines()]}])
+    # X = np.array(X)
+    #
+    # #Teach our ActiveLearner model the record it has requested
+    # learner.teach(X=X, y=y)
+    #
+    # # remove the queried instance from the unlabeled pool
+    # X_pool, y_pool = np.delete(X_pool, query_idx, axis=0), np.delete(y_pool, query_idx)
 
 
 

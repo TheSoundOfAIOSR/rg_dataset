17fc938222f01c392b08e4c88536935d17529264
MKDYasserH <khalafaoui.yasser@gmail.com>
2021-06-08 18:34:35 +0200

    added curated training data

--- reddit_data_preprocessing/pattern_extractor.py
+++ reddit_data_preprocessing/pattern_extractor.py
@@ -1,17 +1,17 @@
 import pandas as pd
 from ast import literal_eval
 
-DATA_PATH = './reddit_data_preprocessing/pf_data.csv'
-OUTPUT_PATH = './reddit_data_preprocessing/pattern_lists.csv'
+DATA_PATH = './reddit_data_preprocessing/data/curated_data.csv'
+OUTPUT_PATH = './reddit_data_preprocessing/data/curated_pattern_lists.csv'
+
 
 def load_data(data_path=DATA_PATH):
-    data = pd.read_csv(data_path, converters={"split_sentences": literal_eval})
-    return data
+    loaded_data = pd.read_csv(data_path, sep=';', converters={"split_sentences": literal_eval})
+    return loaded_data
 
 
 def extract_patterns(sentence):
     '''
-
     :param sentence: list of tuples
     :return: pattern sentence without the actual instruments and qualities. E.g. I heard a QLTY INSTR
              list of instruments
@@ -22,7 +22,6 @@ def extract_patterns(sentence):
     pattern = ''
     instruments = []
     qualities = []
-    edges = []
 
     # extract patterns and append them to lists
     for word, tag in sentence:
@@ -34,30 +33,27 @@ def extract_patterns(sentence):
         elif tag == 'QLTY':
             pattern = pattern + f" {tag}"
             qualities.append(word)
-        elif tag == 'EDGE':
-            pattern = pattern + f" {tag}"
-            edges.append(word)
 
-    return pattern, instruments, qualities, edges
+    return pattern, instruments, qualities
 
 
 def process_data(data, output_path=OUTPUT_PATH):
 
     # create a dataframe to store the results
-    column_names = ['pattern', 'INSTR', 'QLTY', 'EDGE']
+    column_names = ['pattern', 'INSTR', 'QLTY']
     patterns = pd.DataFrame(columns=column_names)
 
     # apply the extract pattern to the column
     for idx, row in data.iterrows():
-        pattern, instruments, qualities, edges = extract_patterns(row['split_sentences'])
+        pattern, instruments, qualities = extract_patterns(row['split_sentences'])
         result_row = {'pattern': pattern,
                       'INSTR': instruments,
-                      'QLTY': qualities,
-                      'EDGE': edges}
+                      'QLTY': qualities}
         patterns = patterns.append(result_row, ignore_index=True)
 
     # save the dataframe into a csv
     patterns.to_csv(path_or_buf=output_path, index=False)
+
 
 if __name__ == '__main__':
     data = load_data()
